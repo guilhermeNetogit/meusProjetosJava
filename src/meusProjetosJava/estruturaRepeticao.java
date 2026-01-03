ef818c9da07fa7813ab69840f0e97a5eeb506c04
guilhermeNetoGit <eng.guilhermeneto@hotmail.com>
2026-01-03 19:21:35 -0300

    novas classes java

---
+++ src/meusProjetosJava/estruturaRepeticao.java
@@ -0,0 +1,13 @@
+package meusProjetosJava;
+
+public class estruturaRepeticao {
+
+    public static void main(String[] args) {
+        // Laço for: inicialização ; condição ; incremento
+        for (int cc = 0;           // cc recebe 0
+             cc < 7;              // executa enquanto cc for menor ou igual a 3
+             cc++) {               // incrementa cc a cada volta
+            System.out.println("Operação " + cc);
+        }
+    }
+}
