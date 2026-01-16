394698772f31d299fad05fdf20766c60444eb515
guilhermeNetoGit <eng.guilhermeneto@hotmail.com>
2026-01-16 19:14:38 -0300

    Update estruturaRepeticao.java

--- src/meusProjetosJava/estruturaRepeticao.java
+++ src/meusProjetosJava/estruturaRepeticao.java
@@ -1,11 +1,11 @@
 package meusProjetosJava;
 
-public class estruturaRepeticao {// @author user Eclipse -> Github @guilhermeNetogit passou aqui em 11/01/2026 21:01:32; Duda 04/01/26 19:16
+public class estruturaRepeticao {// Eclipse <- Github @guilhermeNetogit passou aqui em 16/01/2026 19:14:32; Duda 04/01/26 19:16
 
     public static void main(String[] args) {
         // Laço for: inicialização ; condição ; incremento
         for (int cc = 0;           // cc recebe 0
-             cc < 10;              // executa enquanto cc for menor ou igual a 10
+             cc < 9;              // executa enquanto cc for menor ou igual a 9
              cc++) {             // incrementa cc a cada volta
             System.out.println("Operação " + cc);
         }
