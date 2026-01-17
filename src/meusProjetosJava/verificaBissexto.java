39d1b278cf1cc3a083825882650f9c81d6162ed9
arauj0duy <m.duda.araujo2012@gmail.com>
2026-01-16 21:31:18 -0300

    Last commit today

--- src/meusProjetosJava/verificaBissexto.java
+++ src/meusProjetosJava/verificaBissexto.java
@@ -2,7 +2,7 @@ package meusProjetosJava;
 
 import java.util.Scanner;
 
-public class verificaBissexto {// Eclipse <- Github @arauj0duy passou aqui em 16/01/2026 21:26:54
+public class verificaBissexto {// Eclipse <- Github @arauj0duy passou aqui em 16/01/2026 21:26:54; @guilhermeNetogit 16/01/2026 21:30:29
 	
 	public static void main(String[] args) {
 	      try (Scanner teclado = new Scanner(System.in)) {
@@ -13,7 +13,7 @@ public class verificaBissexto {// Eclipse <- Github @arauj0duy passou aqui em 16
 			 	 int dias[]={31,28,31,30,31,30,31,31,30,31,30,31};
 				String anobi;
 				int anodias;
-			  if ((ano %400 ==0) || ((ano % 4 == 0) && (ano % 100 != 0))){anobi = " é bissexto!";}
+			  if ((ano %400 ==0) || ((ano % 4 == 0) && (ano % 100 != 0))){anobi = " é bissexto";}
 			  else {anobi = " não é bissexto.";}
 			  if (anobi == " é bissexto"){dias[1]=29;anodias=366;}else{dias[1]=28;anodias=365;}
 			  System.out.println("O ano de " +ano + anobi +" e tem " + anodias + " dias."); 
