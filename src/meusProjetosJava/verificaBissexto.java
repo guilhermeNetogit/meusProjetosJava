02561a7b3307c2fc95a24f3857c530e4111a65a1
arauj0duy <m.duda.araujo2012@gmail.com>
2026-01-16 21:26:53 -0300

    Update verificaBissexto.java

--- src/meusProjetosJava/verificaBissexto.java
+++ src/meusProjetosJava/verificaBissexto.java
@@ -2,7 +2,7 @@ package meusProjetosJava;
 
 import java.util.Scanner;
 
-public class verificaBissexto {// @author user Eclipse -> Github @guilhermeNetogit passou aqui em 11/01/2026 21:01:54
+public class verificaBissexto {// Eclipse <- Github @arauj0duy passou aqui em 16/01/2026 21:26:54
 	
 	public static void main(String[] args) {
 	      try (Scanner teclado = new Scanner(System.in)) {
@@ -13,14 +13,14 @@ public class verificaBissexto {// @author user Eclipse -> Github @guilhermeNetog
 			 	 int dias[]={31,28,31,30,31,30,31,31,30,31,30,31};
 				String anobi;
 				int anodias;
-			  if ((ano %400 ==0) || ((ano % 4 == 0) && (ano % 100 != 0))){anobi = " é bissexto";}
-			  else {anobi = " não é bissexto";}
+			  if ((ano %400 ==0) || ((ano % 4 == 0) && (ano % 100 != 0))){anobi = " é bissexto!";}
+			  else {anobi = " não é bissexto.";}
 			  if (anobi == " é bissexto"){dias[1]=29;anodias=366;}else{dias[1]=28;anodias=365;}
 			  System.out.println("O ano de " +ano + anobi +" e tem " + anodias + " dias."); 
 				//for (int c=0;c<=5;c++)
 				for (int c=0;c<mes.length;c++) {
 				  System.out.println("O mês de: " + mes[c] + " tem " + dias[c] + " dias.");
-				}
-		  }
+			}
+		}
 	}
 }
