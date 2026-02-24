32e94613e5b20ffe5509ec91254ed88ebd298c2b
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-02-24 13:22:12 -0300

    feat: Adicionado método com return
    Signed-off-by: guilhermeNetogit <eng.guilhermeneto@hotmail.com>

--- src/meusprojetosjava/Veiculos/TesteVeiculos.java
+++ src/meusprojetosjava/Veiculos/TesteVeiculos.java
@@ -2,17 +2,18 @@ package meusprojetosjava.Veiculos;
 
 class TesteVeiculos {
 
-	public static void main(String[] args) {// Eclipse -> Github @guilhermeNetogit 24/02/2026 11:59:23
+	public static void main(String[] args) {// Eclipse -> Github @guilhermeNetogit 24/02/2026 13:20:58	
 
 		Veiculos onix = new Veiculos();
 
 		onix.marca = "Chevrolet";
-		onix.modelo = "Onix";
+		onix.modelo = "Onix Joy";
 		onix.ano = 2019;
 		onix.numPassageiros = 5;
 		onix.tipoComb = new String[] {Veiculos.Tipo_Comb[2]};
 		onix.capTanqComb = 54;
 		onix.consumoCombustivel = 13.1;
+		onix.valor = 50000;
 		
 		Veiculos fusca = new Veiculos();
 		
@@ -26,19 +27,31 @@ class TesteVeiculos {
 		Veiculos uno = new Veiculos();
 		
 		uno.marca = "Fiat";
-		uno.modelo = "Uno";
+		uno.modelo = "Uno Eletronic";
 		uno.ano = 1994;
 		uno.numPassageiros = 5;
 		uno.tipoComb = new String[] {Veiculos.Tipo_Comb[0]}; 
 		uno.capTanqComb = 50;
 		uno.consumoCombustivel = 10.2;
+		uno.valor = 7000;
 
 		
 		System.out.println(onix);
+		onix.exibirAutonomia();
+		double autonomiaOnix = onix.obterAutonomia();
+		System.out.println("A autonomia do veículo (via return) é: " + autonomiaOnix + " km");
 		System.out.println();
+		
 		System.out.println(fusca);
+		fusca.exibirAutonomia();
+		double autonomiaFusca = fusca.obterAutonomia();
+		System.out.println("A autonomia do veículo (via return) é: " + autonomiaFusca + " km");
 		System.out.println();
+		
 		System.out.println(uno);
+		uno.exibirAutonomia();
+		double autonomiaUno = uno.obterAutonomia();
+		System.out.println("A autonomia do veículo (via return) é: " + autonomiaUno + " km");
 
 	}
 
