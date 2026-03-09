98687c93f52ec3074cd2b846247b78e77cb212e4
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-03-09 16:50:53 -0300

    feat(curso): added a new class
    Signed-off-by: guilhermeNetogit <eng.guilhermeneto@hotmail.com>

--- src/meusprojetosjava/contatos/Contatos.java
+++ src/meusprojetosjava/contatos/Contatos.java
@@ -5,7 +5,12 @@ public class Contatos {// Eclipse -> Github @guilhermeNetogit 08/03/2026 22:20:5
 	private String nome;
 	private Endereco endereco;
 	private Telefone[] telefones;
+	private String email;
 	
+	public Contatos() {
+		
+	}
+
 	public String getNome() {
 		return nome;
 	}
@@ -19,10 +24,20 @@ public class Contatos {// Eclipse -> Github @guilhermeNetogit 08/03/2026 22:20:5
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+	
 	public Telefone[] getTelefones() {
 		return telefones;
 	}
 	public void setTelefones(Telefone[] telefones) {
 		this.telefones = telefones;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
