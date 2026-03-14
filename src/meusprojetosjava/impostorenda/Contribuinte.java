ff18f98c7783c06db39f0337f52ac0f8d8952e52
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-03-14 19:17:36 -0300

    feat(ImpostoRenda): add new class for calculating income tax.

---
+++ src/meusprojetosjava/impostorenda/Contribuinte.java
@@ -0,0 +1,32 @@
+package meusprojetosjava.impostorenda;
+
+public abstract class Contribuinte {// Eclipse -> Github @guilhermeNetogit 14/03/2026 19:07:06
+	
+	String nome;
+	double rendaBruta;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getRendaBruta() {
+		return rendaBruta;
+	}
+	public void setRendaBruta(double rendaBruta) {
+		this.rendaBruta = rendaBruta;
+	}
+	
+	public abstract double calcularIR();
+	
+	public abstract String gerarRelatorio();
+	
+	@Override
+	public String toString() {
+		return String.format("Nome: %s", nome);
+		
+	}
+	
+}
