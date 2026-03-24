902aee5b016a1c390e4c4c2da0bb332f121216b3
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-03-24 20:44:42 -0300

    refactor(): update to display output in MB and Bytes
    Also added is double execution of the .gc file to increase accuracy.

--- src/meusprojetosjava/GarbageCollector.java
+++ src/meusprojetosjava/GarbageCollector.java
@@ -1,47 +1,50 @@
 package meusprojetosjava;
 
 import meusprojetosjava.contatos.contatos2.Contatos2;
+import java.util.Arrays;
 
-public class GarbageCollector {// Eclipse -> Github @guilhermeNetogit 24/03/2026 19:43:08
+public class GarbageCollector {// Eclipse -> Github @guilhermeNetogit 24/03/2026 20:33:26
 
-	public static void obterMemoriaUsada() {
+	public static double obterMemoriaUsada() {
 
 		final double MB = 1024 * 1024.0;
 
 		Runtime runtime = Runtime.getRuntime();
+		long usadoByte = (runtime.totalMemory() - runtime.freeMemory());
+		double usadoMB = usadoByte / MB;
 
-		System.out.println((runtime.totalMemory() - runtime.freeMemory()) / MB + " MB");
-		System.out.println((runtime.totalMemory() - runtime.freeMemory()) + " Byte");
+		System.out.printf("%.6f MB%n", usadoMB);
+		System.out.printf("%,d Bytes%n", usadoByte);
+		return usadoMB;
 	}
 
-	@SuppressWarnings("removal")
 	public static void main(String[] args) {
 
 		Contatos2[] contatos = new Contatos2[100000];
-		Contatos2 contato;
 
 		for (int i = 0; i < contatos.length; i++) {
-			contato = new Contatos2();
-			contato.setTipoContato(new String[] { Contatos2.TIPO_CONTATO[2] });
-			contato.setNome("Contato" + i);
-			contato.setTelefones(new String[] { "123-4567" + i });
-			contato.setEmail("contato" + i + "@email.com");
-			contatos[i] = contato;
+			contatos[i] = new Contatos2(
+					new String[] { Contatos2.TIPO_CONTATO[2] }, 
+					"Contato" + i, 
+					"Rua" + i,
+					new String[] { "123-4567" + i }, 
+					"contato" + i + "@email.com");
 		}
 
-		System.out.println("Contatos criados");
-
-		obterMemoriaUsada();
+		long criados = Arrays.stream(contatos).filter(c -> c != null).count();
+		System.out.println("Contatos criados -> " + criados);
+		
+		double antes = obterMemoriaUsada();
 
 		contatos = null;
 
-		Runtime.getRuntime().runFinalization();
-
 		Runtime.getRuntime().gc();
 
-		System.out.println("Contatos removidos");
+		System.out.println("\nContatos removidos -> ");
+		double depois = obterMemoriaUsada();
 
-		obterMemoriaUsada();
+		System.out.printf("\nTotal liberado: %.2f MB%n", antes - depois);
+
 	}
 
 }
