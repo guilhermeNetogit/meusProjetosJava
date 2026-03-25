f1612b6d33ff540680a9b5b177e660aae94dc4f8
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-03-25 18:33:21 -0300

    feat(new class): add a new thread class as a synchronized block.

---
+++ src/meusprojetosjava/threads/thread4/Calculadora.java
@@ -0,0 +1,28 @@
+package meusprojetosjava.threads.thread4;
+
+public class Calculadora {// Eclipse -> Github @guilhermeNetogit 25/03/2026 18:30:43
+
+	private int soma;
+
+	public synchronized int somaArray(int[] array) {
+
+		soma = 0;
+
+		for (int i = 0; i < array.length; i++) {
+
+			soma += array[i];
+
+			System.out.println("Executando a soma " + Thread.currentThread().getName() + " somando o valor " + array[i]
+					+ " com total de " + soma);
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		return soma;
+	}
+}
